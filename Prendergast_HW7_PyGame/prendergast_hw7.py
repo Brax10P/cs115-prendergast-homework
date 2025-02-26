ecf8ed3bc4ebc6996b14d55c840ddc68a8597de9
Brax10P <bprendergast12@hotmail.com>
2025-02-26 19:18:04 +0000

    Added pygames and file for frogger, started changing shapes to create the map

---
+++ Prendergast_HW7_PyGame/prendergast_hw7.py
@@ -0,0 +1,65 @@
+import pygame
+
+#init pygame
+pygame.init() 
+
+#window dimensions
+width = 600
+height = 400
+screen = pygame.display.set_mode((width,height))
+
+#set window title
+pygame.display.set_caption("Frogger")
+
+#fps
+clock = pygame.time.Clock()
+dt = 0
+speed = 10
+
+""" Game Loop """
+running = True
+while running:
+  """Handle Events"""
+  for event in pygame.event.get():
+    if event.type == pygame.quit:
+      running = False
+
+
+  """Update Our Game State"""
+
+
+  """Draw to our Screen"""
+  #clear screen
+  screen.fill((128, 128, 128))
+
+  #draw rectangle
+  pygame.draw.rect(
+    screen, 
+    "blue", 
+    pygame.Rect((0,375), (800,25)))
+
+  #Draw Circle
+  pygame.draw.circle(
+    screen, 
+    "green", 
+    (100,200), 
+    40)
+
+  #Draw a Line
+  pygame.draw.line(
+    screen, 
+    "red", 
+    (100,100), 
+    (200,200), 
+    5)
+
+
+  #update screen
+  pygame.display.flip()
+
+  #fps
+  dt = clock.tick(speed)/1000
+
+#quit pygame
+pygame.quit()
+
